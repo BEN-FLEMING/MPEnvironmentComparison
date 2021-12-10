84cb8a00af6ccd690540d5eb5a54bfab249edc34
Your Name <itsbenfleming@gmail.com>
2021-12-10 15:22:49 +0000

    changes made

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,20 @@
-from collections import namedtuple
-import altair as alt
-import math
 import pandas as pd
 import streamlit as st
+from gsheetsdb import connect
 
-st.title('Uber pickups in NYC')
+st.title('MP Eco Score')
+
+conn = connection()
+
+@st.cache(ttl=600)
+def run_query(query):
+    rows = conn.execute(query, headers=1)
+    return rows
+
+sheet_url = st.secrets["public_gsheets_url"]
+
+rows = run_query(f'SELECT * FROM "{sheet_url}"')
+
+# Print results.
+for row in rows:
+    st.write(f"{row.Constituency} has a :{row.Total}:")
