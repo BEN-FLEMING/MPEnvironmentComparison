fd02b4a858c4f2ec7ced3ea084f461aea2221118
Your Name <itsbenfleming@gmail.com>
2021-12-14 12:21:16 +0000

    added multiselect

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@ from numpy.core.fromnumeric import argsort
 import pandas as pd
 import streamlit as st
 import matplotlib.pyplot as plt
+import altair as alt
 from gsheetsdb import connect
 
 st.set_page_config(
@@ -30,12 +31,35 @@ rows = run_query(f'SELECT * FROM "{sheet_url}"')
 
 df = pd.DataFrame(rows)
 
-def onSelect(*options):
+#columns=['Constituency','MP_Name','Supports_the_CEE_Bill?','Attended_Climate_Change_Debate?','Signed_Net_Zero_Letter?','Transport Emissions Target','Voted_for_2015_Fracking_Moratorium','2008_Climate_Change_Act','Prevent_Onshore Wind_Subsidy_Abolition','Supports_Divest_Parliament','GIB Climate_Target','Energy Decarbonisation_Target_2013','Energy_Decarbonisation_Target_2016','Total','Total_Possible_Votes','Total_Score']
+
+df.set_index("Constituency")
+
+plot = alt.Chart(df).mark_bar().encode(
+        x='Constituency',
+        y='Total_Score',
+    )
+
+st.write(plot)
+
+options = []
+
+def onSelect():
     for x in options:
         st.write("You have selected:" + x)
 
+    result = df.loc[options]
+    
+    
+    plot1 = alt.Chart(result).mark_bar().encode(
+        x='Constituency',
+        y='Total_Score',
+    )
+    
+    st.write(plot1)
 
-options = st.multiselect("Select consituencies",df,on_change=onSelect())
+
+options = st.multiselect("Select consituencies",df,on_change=onSelect)
 
 
 """
